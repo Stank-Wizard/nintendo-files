dc06cad5832441f8122efca16bff913f942b194d
StankWizardLizard <bukki2002@gmail.com>
2025-12-22 10:49:22 +0000

    minor changes to readme and refactor of code

--- bntx/main.cpp
+++ bntx/main.cpp
@@ -3,45 +3,27 @@
 #include <iomanip>
 #include "../utils/utils.hpp"
 
-#define bntx 1112429656
-#define nx 1314398240
-#define str 1599296594
-#define brti 1112691785
-#define brtd 1111691780
-#define rlt 1599299012
-#define dic 1598310723
+#define BNTX 1112429656
+#define NX 1314398240
+#define STR 1599296594
+#define BRTI 1112691785
+#define BRTD 1111691780
+#define RLT 1599299012
+#define DIC 1598310723
 
 using namespace std;
 
-int copy_section(char* src_buffer, int src_buffer_pos, char* dest_buffer, int dest_buffer_size) {
-
-    // Iterate through each item in src_buffer and assign value to dest_buffer
-    for(int i = 0; src_buffer_pos+i < (src_buffer_pos+dest_buffer_size); i++) {
-        dest_buffer[i] = src_buffer[src_buffer_pos+i];
-    }
-
-    return 0;
-}
-
-int print_bntx_header(char* src_buffer) {
-
-    unsigned int bntx_magic_number = be_cast_int(src_buffer, 0x0);
-
-    if(bntx_magic_number != bntx) {
-        cerr << "[!] Error not a bntx file! " << endl;
-
-        return 1;
-    }
+int print_bntx_header(char* src_buffer, int bntx_offset) {
 
     // Extract important data from headers
-    long version = le_cast_long(src_buffer, 0x4);
-    short byte_order_mark = le_cast_short(src_buffer, 0xC);
-    short alignment_exponent = le_cast_short(src_buffer, 0xE);
-    int file_name_offset = le_cast_int(src_buffer, 0x10);
-    short is_relocated = le_cast_short(src_buffer, 0x14);
-    short str_table_offset = le_cast_short(src_buffer, 0x16);
-    int rlt_table_offset = le_cast_int(src_buffer, 0x18);
-    int file_size = le_cast_int(src_buffer, 0x1C);
+    long version = le_cast_long(src_buffer, bntx_offset + 0x4);
+    short byte_order_mark = le_cast_short(src_buffer, bntx_offset + 0xC);
+    short alignment_exponent = le_cast_short(src_buffer, bntx_offset + 0xE);
+    int file_name_offset = le_cast_int(src_buffer, bntx_offset + 0x10);
+    short is_relocated = le_cast_short(src_buffer, bntx_offset + 0x14);
+    short str_table_offset = le_cast_short(src_buffer, bntx_offset + 0x16);
+    int rlt_table_offset = le_cast_int(src_buffer, bntx_offset + 0x18);
+    int file_size = le_cast_int(src_buffer, bntx_offset + 0x1C);
 
     cout << "BNTX : Version : 0x" << hex << version << endl;
     cout << "BNTX : BOM : 0x" << hex << byte_order_mark << endl;
@@ -55,21 +37,13 @@ int print_bntx_header(char* src_buffer) {
     return 0;    
 }
 
-int print_nx_header(char* src_buffer) {
-
-    unsigned int nx_magic_number = be_cast_int(src_buffer, 0x0);
-
-    if(nx_magic_number != nx) {
-        cerr << "[!] Error doesnt contain a nx texture container! " << endl;
-
-        return 1;
-    }
+int print_nx_header(char* src_buffer, int nx_offset) {
 
     // Extract important data from headers
-    int nx_number_of_files = le_cast_int(src_buffer, 0x4);
-    long brti_address_table_offset = le_cast_long(src_buffer, 0x8);
-    long brtd_table_offset = le_cast_long(src_buffer, 0x10);
-    long dic_table_offset = le_cast_long(src_buffer, 0x18);
+    int nx_number_of_files = le_cast_int(src_buffer, nx_offset + 0x4);
+    long brti_address_table_offset = le_cast_long(src_buffer, nx_offset + 0x8);
+    long brtd_table_offset = le_cast_long(src_buffer, nx_offset + 0x10);
+    long dic_table_offset = le_cast_long(src_buffer, nx_offset + 0x18);
 
     cout << "NX : Number of files: " << nx_number_of_files << endl;
     cout << "NX : BRTI Address table offset: 0x" << hex << brti_address_table_offset << dec << endl;
@@ -79,21 +53,11 @@ int print_nx_header(char* src_buffer) {
     return 0;
 }
 
-int print_str_header(char* src_buffer) {
-
-    // Extract magic number from header
-    unsigned int str_magic_number = be_cast_int(src_buffer, 0x0);
-
-    // Magic number check
-    if(str_magic_number != str) {
-        cerr << "[!] Error doesnt contain a str table! " << endl;
-
-        return 1;
-    }
+int print_str_header(char* src_buffer, int str_offset) {
 
     // Extract important data from headers
-    long str_next_section_offset = le_cast_long(src_buffer, 0x8);
-    long str_number_of_files = le_cast_long(src_buffer, 0x10);
+    long str_next_section_offset = le_cast_long(src_buffer, str_offset + 0x8);
+    long str_number_of_files = le_cast_long(src_buffer, str_offset + 0x10);
     
     cout << "STR : Next section offset: 0x" << hex << str_next_section_offset << endl;
     cout << "STR : Number of files: " << dec << str_number_of_files << endl;
@@ -101,22 +65,29 @@ int print_str_header(char* src_buffer) {
     return 0;
 }
 
-int print_dic_header(char* src_buffer) {
+int print_str_table(char* src_buffer, int str_offset) {
+    return 0;
+}
 
-    unsigned int dic_magic_number = be_cast_int(src_buffer, 0x0);
-
-    // Magic number check
-    if(dic_magic_number != dic) {
-        cerr << "[!] Error doesnt contain a dir table! " << endl;
-
-        return 1;
-    }
+int print_dic_header(char* src_buffer, int dic_offset) {
 
     // Extract important data from headers
-    int dic_number_of_files = le_cast_int(src_buffer, 0x4);
+    int dic_number_of_files = le_cast_int(src_buffer, dic_offset + 0x4);
 
     cout << "DIC : Number of files: " << dec << dic_number_of_files << endl;
 
+    return 0;
+}
+
+int print_dic_table(char* src_buffer, int dic_offset) {
+    return 0;
+}
+
+int print_brti_header(char* src_buffer) {
+    return 0;
+}
+
+int print_brtd_header(char* src_buffer) {
     return 0;
 }
 
@@ -133,55 +104,101 @@ int read_bntx(string file_path_in) {
 
     // Get file size and allocate memory for contents
     unsigned int bntx_data_size = input_stream.tellg();
-
-    // Allocate array in memory for file contents
     char *bntx_data = new char[bntx_data_size];
 
-    // Go back to begining of file
+    // Go back to begining of file and read into buffer
     input_stream.seekg(0, ios::beg);
-
-    // Read contents of file into memory
     input_stream.read(bntx_data, bntx_data_size);
-
-    // Close file stream
     input_stream.close();
 
     cout << file_path_in << endl;
 
     // Print out each header
+
+    // --------------- BNTX HEADER -------------------
     unsigned int bntx_header_size = 0x20;
-    char *bntx_header = new char[bntx_header_size];
-    copy_section(bntx_data, 0x0, bntx_header, bntx_header_size);
-    short str_table_offset = le_cast_short(bntx_header, 0x16);
-    int rlt_table_offset = le_cast_int(bntx_header, 0x18);
-    print_bntx_header(bntx_header);
-    delete[] bntx_header;
-    bntx_header = nullptr;
+    unsigned int bntx_header_offset = 0x0;
+    unsigned int bntx_magic_number = be_cast_int(bntx_data, bntx_header_offset);
 
+    if(bntx_magic_number != BNTX) {
+        cerr << "[!] Error, Doesn't contain valid BNTX section! " << file_path_in << endl;
+
+        delete[] bntx_data;
+        bntx_data = nullptr;
+
+        return 1;
+    }
+
+    print_bntx_header(bntx_data, bntx_header_offset);
+
+    short str_header_offset = le_cast_short(bntx_data, bntx_header_offset + 0x16);
+    int rlt_header_offset = le_cast_int(bntx_data, bntx_header_offset + 0x18);
+
+    // --------------- NX HEADER -------------------
     unsigned int nx_header_size = 0x20;
-    char *nx_header = new char[nx_header_size];
-    copy_section(bntx_data, 0x20, nx_header, nx_header_size);
-    long brti_address_table_offset = le_cast_long(nx_header, 0x8);
-    long brtd_table_offset = le_cast_long(nx_header, 0x10);
-    long dic_table_offset = le_cast_long(nx_header, 0x18);
-    print_nx_header(nx_header);
-    delete[] nx_header;
-    nx_header = nullptr;
+    unsigned int nx_header_offset = bntx_header_offset + bntx_header_size;
+    unsigned int nx_magic_number = be_cast_int(bntx_data, nx_header_offset);
 
-    unsigned int str_header_size = 0x18;
-    char *str_header = new char[str_header_size];
-    copy_section(bntx_data, str_table_offset, str_header, str_header_size);
-    print_str_header(str_header);
-    delete[] str_header;
-    str_header = nullptr;
+    if(nx_magic_number != NX) {
+        cerr << "[!] Error, Doesn't contain valid NX section! " << file_path_in << endl;
 
-    unsigned int dic_header_size = 0x14;
-    char *dic_header = new char[dic_header_size];
-    copy_section(bntx_data, dic_table_offset, dic_header, dic_header_size);
-    print_dic_header(dic_header);
-    delete[] dic_header;
-    dic_header = nullptr;
+        delete[] bntx_data;
+        bntx_data = nullptr;
+
+        return 1;
+
+    }
+
+    print_nx_header(bntx_data, nx_header_offset);
+
+    long brti_header_offset = le_cast_long(bntx_data, nx_header_offset + 0x8);
+    long brtd_header_offset = le_cast_long(bntx_data, nx_header_offset + 0x10);
+    long dic_header_offset = le_cast_long(bntx_data, nx_header_offset + 0x18);
+    /*
+    long brti_address_table_offset = le_cast_long(nx_header, nx_header_offset + 0x8);
+    long brtd_table_offset = le_cast_long(nx_header, nx_header_offset + 0x10);
+    long dic_table_offset = le_cast_long(nx_header, nx_header_offset + 0x18);
+     */
     
+     // --------------- _STR HEADER -------------------
+    unsigned int str_header_size = 0x18;
+    unsigned int str_table_offset = str_header_offset + str_header_size;
+    unsigned int str_magic_number = be_cast_int(bntx_data, str_header_offset);
+
+    if(str_magic_number != STR) {
+        cerr << "[!] Error, Doesn't contain valid _STR section! " << file_path_in << endl;
+
+        delete[] bntx_data;
+        bntx_data = nullptr;
+
+        return 1;
+
+    }
+
+    print_str_header(bntx_data, str_header_offset);
+    print_str_table(bntx_data, str_table_offset);
+
+    // --------------- _DIC HEADER -------------------
+    unsigned int dic_header_size = 0x14;
+    unsigned int dic_table_offset = dic_header_offset + dic_header_size;
+    unsigned int dic_magic_number = be_cast_int(bntx_data, dic_header_offset);
+
+    if(dic_magic_number != DIC) {
+        cerr << "[!] Error, Doesn't contain valid DIC section! " << file_path_in << endl;
+
+        delete[] bntx_data;
+        bntx_data = nullptr;
+
+        return 1;
+    }
+
+    print_dic_header(bntx_data, dic_header_offset);
+    print_dic_table(bntx_data, dic_table_offset);
+    
+    // --------------- BRTI HEADER -------------------
+    // --------------- BRTD HEADER -------------------
+    // --------------- _RLT HEADER -------------------
+
     delete[] bntx_data;
     bntx_data = nullptr;
 
@@ -209,20 +226,7 @@ int main() {
     read_bntx("systemDataUnpacked/Set/timg/__Combined.bntx");
     */
 
-    read_bntx("systemDataUnpacked/common/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Entrance/timg/__Combined.bntx");
     read_bntx("systemDataUnpacked/Eula/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Flaunch/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Gift/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Interrupt/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Migration/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/MyPage/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Notification/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Option/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Psl/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/ResidentMenu/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/SaveMove/timg/__Combined.bntx");
-    read_bntx("systemDataUnpacked/Set/timg/__Combined.bntx");
 
     return 0;
 }
--- bntx/readme.md
+++ bntx/readme.md
@@ -11,8 +11,8 @@ A BNTX file contains textures
 | 0xE | 2 | Alignment exponent |
 | 0x10 | 2 | File name target address |
 | 0x14 | 2 | Is relocated |
-| 0x16 | 2 | _STR table offset |
-| 0x18 | 4 | _RLT table offset |
+| 0x16 | 2 | [_STR table](#_str-header) offset |
+| 0x18 | 4 | [_RLT table](#_rlt-header) offset |
 | 0x1C | 4 | File size |
 
 # NX Header
@@ -23,9 +23,9 @@ the texture container itself
 | :--- | :--- | :--- |
 | 0x0 | 4 | Magic number (NX) |
 | 0x4 | 4 | Number of files |
-| 0x8 | 8 | Texture table offset, points to table of 64 bit pointers into BRTI sections |
-| 0x10 | 8 | Offset into BRTD section |
-| 0x18 | 8 | Offset into _DIC section |
+| 0x8 | 8 | Texture table offset, points to table of 64 bit pointers into [BRTI tables](#brti-header) |
+| 0x10 | 8 | [BRTD table](#brtd-header) offset |
+| 0x18 | 8 | [_DIC table](#_dic-header) offset |
 
 # _STR Header
 
@@ -34,7 +34,10 @@ String Table
 | Offset | Size | Description |
 | :--- | :--- | :--- |
 | 0x0 | 4 | Magic number (_STR) |
-| 0x0 | 4 |  |
+| 0x4 | 4 | Next section offset |
+| 0x8 | 8 | Section size (including the following _DIC) |
+| 0x10 | 8 | Number of strings in table |
+| 0x18 | N | Null-terminated file names |
 
 # _DIC Header
 
@@ -43,9 +46,12 @@ Dictionary
 | Offset | Size | Description |
 | :--- | :--- | :--- |
 | 0x0 | 4 | Magic number (_DIC) |
-| 0x0 | 4 |  |
+| 0x4 | 4 | Number of entries |
+| 0x4 | 4 |  |
 
 # BRTI header
+
+Texture info
 
 | Offset | Size | Description |
 | :--- | :--- | :--- |
@@ -53,6 +59,8 @@ Dictionary
 | 0x0 | 4 |  |
 
 # BRTD Header
+
+Texture data
 
 | Offset | Size | Description |
 | :--- | :--- | :--- |
